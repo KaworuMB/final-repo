461947f3bc1ab75288a369e7307f816100f37259
KaworuMB <mmoldabekbekzat@gmail.com>
2025-05-12 10:07:22 +0500

    Update LoginPage.js

--- webback_frontend/src/components/LoginPage.js
+++ webback_frontend/src/components/LoginPage.js
@@ -20,7 +20,7 @@ const LoginPage = () => {
 
   const handleSubmit = async e => {
     e.preventDefault();
-    const response = await fetch('http://kaworu.kz/api/auth/login/', {
+    const response = await fetch('https://kaworu.kz/api/auth/login/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(form),
